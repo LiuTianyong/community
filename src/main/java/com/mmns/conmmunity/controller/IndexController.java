ca69288faf3b1a3faaf5e18a40da2f5ffe570b48
LiuTianyong <10633331689@qq.com>
2020-02-07 21:03:21 +0800

    add index and bootstrap

--- src/main/java/com/mmns/conmmunity/controller/IndexController.java
+++ src/main/java/com/mmns/conmmunity/controller/IndexController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController {
+public class IndexController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+    @GetMapping("/")
+    public String index(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "index";
     }
 
 }
