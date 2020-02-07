ccbb7e3fe65313db3d0ad0154b987a0192122b19
LiuTianyong <10633331689@qq.com>
2020-02-07 18:52:53 +0800

    first commit

---
+++ src/main/java/com/mmns/conmmunity/ConmmunityApplication.java
@@ -0,0 +1,13 @@
+package com.mmns.conmmunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConmmunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConmmunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mmns/conmmunity/ConmmunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmns.conmmunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConmmunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
